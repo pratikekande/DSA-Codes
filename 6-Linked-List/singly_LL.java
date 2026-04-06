ad2f0eac5279193c658cdb811c88163de3a91c37
pratikekande <pratikekande11@gmail.com>
2026-04-06 11:17:33 +0530

    Singly and Doubly Linked List

---
+++ 6-Linked-List/singly_LL.java
@@ -0,0 +1,237 @@
+import java.util.Scanner;
+
+class Node{
+    int data;
+    Node next = null;
+
+    Node(int data){
+        this.data = data;
+    }
+}
+
+class LinkedList{
+
+    Node head = null;
+
+    // ADD FIRST
+    void addFirst(int data){
+        Node newNode = new Node(data);
+        if(head == null){
+            head = newNode;
+        } else{
+            newNode.next = head;
+            head = newNode;
+        }
+    }
+
+    // ADD LAST
+    void addLast(int data){
+        Node newNode = new Node(data);
+        if(head == null){
+            head = newNode;
+        } else{
+            Node temp = head;
+            while (temp.next != null) {
+                temp = temp.next;
+            }
+            temp.next = newNode;
+        }
+    }
+
+    // COUNT NODE
+    int countNode(){
+        int count = 0;
+        Node temp = head;
+        while (temp != null) {
+            count++;
+            temp = temp.next;
+        }
+        return count;
+    }
+
+    // INSERT AT POSITION
+    void addAtPos(int pos, int data){
+        int n = countNode();
+
+        if(pos <= 0 || pos > n+1){
+            System.out.println("Wrong Input for position");
+            return;
+        }
+
+        if(pos == 1){
+            addFirst(data);
+        } else if(pos == n+1){
+            addLast(data);
+        }else{
+            Node newNode = new Node(data);
+            Node temp = head;
+            while (pos-2 != 0) {    // can also write while(pos-1 != 1) 
+                temp = temp.next;
+                pos--;
+            }
+            newNode.next = temp.next;
+            temp.next = newNode;
+        }
+    }
+
+    // DELETE FIRST
+    void deleteFirst(){
+        if(head == null){
+            System.out.println("Node is not present for deletion");
+        } else{
+            head = head.next;
+        }
+    }
+
+    // DELETE LAST
+    void deleteLast(){
+        if(head == null){
+            System.out.println("Node is not present for deletion");
+            return;
+        }
+
+        if(countNode() == 1){
+            head = null;
+        }else{
+            Node temp = head;
+
+            while (temp.next.next != null) {
+                temp = temp.next;
+            }
+            temp.next = null;
+        }
+    }
+
+    // DELETE AT POSITION
+    void deleteAtPos(int pos){
+        int n = countNode();
+
+        if(pos <= 0 || pos > n){
+            System.out.println("Wrong Input for position");
+            return;
+        }
+
+        if(pos == 1){
+            deleteFirst();
+        } else if(pos == n){
+            deleteLast();
+        }else{
+            Node temp = head;
+            while (pos-2 != 0) {
+                temp = temp.next;
+                pos--;
+            }
+            temp.next = temp.next.next;
+        }
+    }
+
+    // PRINT LINKED LIST
+    void printLinkedList(){
+        if(head == null){
+            System.out.println("LinkedList is empty");
+            return;
+        } else{
+            Node temp = head;
+            while (temp.next != null) {
+                System.out.print(temp.data + "->");
+                temp = temp.next;
+            }
+            System.out.println(temp.data);
+        }
+    }
+}
+
+class Client{
+
+    public static void main(String[] args) {
+        
+        LinkedList ll = new LinkedList();
+
+        char ch;
+
+        do{
+            System.out.println("Singly LinkedList");
+            System.out.println("1. Add First");
+            System.out.println("2. Add Last");
+            System.out.println("3. Add at Position");
+            System.out.println("4. Delete First");
+            System.out.println("5. Delete Last");
+            System.out.println("6. Delete at Postion");
+            System.out.println("7. Count Node");
+            System.out.println("8. Print LinkedList");
+
+            System.out.println("Enter your choice :");
+
+            Scanner sc = new Scanner(System.in);
+
+            int choice = sc.nextInt();
+
+            switch (choice) {
+                case 1:{
+                        System.out.println("Enter data");
+                        int data = sc.nextInt();
+                        ll.addFirst(data);
+                    }   
+                    break;
+                case 2:{
+                        System.out.println("Enter data");
+                        int data = sc.nextInt();
+                        ll.addLast(data);
+                    }     
+                    break;
+                case 3:{
+                        System.out.println("Enter data");
+                        int data = sc.nextInt();
+                        System.out.println("Enter Position");
+                        int pos = sc.nextInt();
+                        ll.addAtPos(pos, data);
+                    }     
+                    break;
+                case 4:
+                    ll.deleteFirst();
+                    break;
+                case 5:
+                    ll.deleteLast();
+                    break;
+                case 6:{
+                        System.out.println("Enter Position");
+                        int pos = sc.nextInt();
+                        ll.deleteAtPos(pos);
+                    }     
+                    break;
+                case 7:
+                    ll.countNode();
+                    break;
+                case 8:
+                    ll.printLinkedList();
+                    break;
+                default:
+                    System.out.println("Wrong Input");
+            }
+
+            System.out.println("Do you want to continue?");
+            ch = sc.next().charAt(0);
+        }while(ch == 'y' || ch == 'Y');
+
+        // ll.printLinkedList();
+
+        // ll.addFirst(30);
+        // ll.addFirst(20);
+        // ll.addFirst(10);
+        // ll.printLinkedList();
+
+        // ll.addLast(40);
+        // ll.printLinkedList();
+
+        // ll.addAtPos(2, 25);
+        // ll.printLinkedList();
+
+        // ll.deleteFirst();
+        // ll.printLinkedList();
+
+        // ll.deleteLast();
+
+        // ll.deleteAtPos(2);
+        // ll.printLinkedList();
+    }
+}
